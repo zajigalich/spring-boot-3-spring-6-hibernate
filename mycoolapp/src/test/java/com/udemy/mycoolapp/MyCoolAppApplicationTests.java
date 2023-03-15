cd997d59cc91ef63b21b29e9b460089f3c9dfd3b
Konstantin <titarenko42k@gmail.com>
2023-03-15 23:40:10 +0200

    Basic Program using @SpringBootApplication

---
+++ mycoolapp/src/test/java/com/udemy/mycoolapp/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
