1a8c4a427d9d96aaca43c6d835c2e4915918087e
Konstantin <titarenko42k@gmail.com>
2023-03-16 02:39:37 +0200

    delete unnecessary

---
+++ mycoolapp/src/main/java/com/udemy/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.udemy.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String satHello(){
+        return "Hello world";
+    }
+}
