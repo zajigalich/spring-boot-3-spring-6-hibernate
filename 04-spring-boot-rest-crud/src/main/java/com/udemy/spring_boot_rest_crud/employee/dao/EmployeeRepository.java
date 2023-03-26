aa22006266b9255ad7ad86c81c97604ad7b3339c
Konstantin <titarenko42k@gmail.com>
2023-03-26 22:51:05 +0300

    04-spring-boot-rest-crud - add spring data rest dependency(provides default endpoints for crud, no need for rest controller and service layer); update application.properties

--- 04-spring-boot-rest-crud/src/main/java/com/udemy/spring_boot_rest_crud/employee/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/src/main/java/com/udemy/spring_boot_rest_crud/employee/dao/EmployeeRepository.java
@@ -2,8 +2,9 @@ package com.udemy.spring_boot_rest_crud.employee.dao;
 
 import com.udemy.spring_boot_rest_crud.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+//@Repository - optional
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
