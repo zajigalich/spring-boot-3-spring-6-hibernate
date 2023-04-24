d7ac72b12167b11cdc7ef5fdbd8156b1989745f0
Konstantin <titarenko42k@gmail.com>
2023-04-24 20:22:49 +0300

    05-spring-boot-rest-security - database bcrypt authorization

--- 05-spring-boot-rest-security/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
+++ 05-spring-boot-rest-security/sql-scripts/05-setup-spring-security-demo-database-bcrypt.sql
@@ -27,7 +27,7 @@ CREATE TABLE `users` (
 INSERT INTO `users` 
 VALUES 
 ('john','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
-('mary','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1),
+('mary','{bcrypt}$2a$10$0cy6BV0pUp.6ewvlTb.aS.9Wl6Wo5P/o6GZCDlCf0.WF43xCbnepu',1),
 ('susan','{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',1);
 
 
