8b64fd6ab62d14cf5321f69f8918a3ef9580e9d8
Konstantin <titarenko42k@gmail.com>
2023-04-24 19:57:50 +0300

    05-spring-boot-rest-security - database plaintext authorization

--- 05-spring-boot-rest-security/src/main/java/com/example/spring_boot_rest_security/security/SecurityConfig.java
+++ 05-spring-boot-rest-security/src/main/java/com/example/spring_boot_rest_security/security/SecurityConfig.java
@@ -5,16 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
         UserDetails john = User.builder()
@@ -35,9 +41,8 @@ public class SecurityConfig {
                 .authorities("ROLE_EMPLOYEE", "ROLE_MANAGER", "ROLE_ADMIN")
                 .build();
 
-
         return new InMemoryUserDetailsManager(john, mary, susan);
-    }
+    }*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
